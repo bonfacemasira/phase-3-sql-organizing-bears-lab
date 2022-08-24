197b2f6d4de350b586ec873d890bc816c5fe4338
Bonface Masira <bonfacemasira@gmail.com>
2022-08-25 00:51:55 +0300

    commit tree 184e7517b1166891d9d6c540de633b0412aa3887

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 4, 'M', 'Red', 'Honesty', 'True');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 4, 'M', 'Blue', 'Fair', 'False');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 5, 'F', 'Indigo', 'Inspiring', 'True');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 1, 'M', 'Red', 'Moving', 'False');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 7, 'F', 'Blue', 'Honesty', 'True');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 4, 'M', 'Yellow', 'Ready', 'False');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 2, 'F', 'Maroon', 'Prepared', 'True');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 1, 'F', 'Maroon', 'Prepared', 'True');
